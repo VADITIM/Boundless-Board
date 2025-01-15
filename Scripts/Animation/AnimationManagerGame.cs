a9fcfe453e84589c6c6009770dc96bdd9ee5e285
Vadim Niedental <vadim.niedental@gmail.com>
2025-01-15 21:32:57 +0100

    Animation handlers for main menu and game scene.

---
+++ Scripts/Animation/AnimationManagerGame.cs
@@ -0,0 +1,162 @@
+using UnityEngine;
+using TMPro;
+using DG.Tweening;
+
+public class AnimationManagerGame : MonoBehaviour
+{
+    private float elapsedTime = 0f;
+    private float fadeOutDuration = 1f;
+
+    [SerializeField] private RectTransform exitButton;
+    private Vector2 exitButtonInitialPosition = new Vector2(1000f, 310f);
+    private Vector2 exitButtonFinalPosition = new Vector2(360f, 310f);
+    private float buttonTweenDuration = 1.5f;
+    private float exitButtonFloatSpeed = .3f;
+    private float exitButtonFloatAmplitude = 7f;
+    private bool exitButtonHasEntered;
+
+    // ------------------------------------------------------------------------------------------------------
+
+    [SerializeField] private RectTransform ticTacToeBoard;
+    private float ticTacToeBoardInitialPositionY = 1000f;
+    private float ticTacToeBoardFinalPositionY = 0f;
+    private float ticTacToeBoardTweenDuration = 1.5f;
+
+    // ------------------------------------------------------------------------------------------------------
+
+    [SerializeField] private RectTransform score;
+    private Vector2 scoreInitialPosition = new Vector2(1000f, 310f);
+    private Vector2 scoreFinalPosition;
+    private float scoreTweenDuration = 1.65f;
+    private float scoreFloatSpeed = 1.3f;
+    private float scoreFloatAmplitude = 12f;
+    private bool scoreHasEntered;
+    private bool isRotating;
+
+    // ------------------------------------------------------------------------------------------------------
+
+    [SerializeField] private RectTransform turn;
+    private Vector2 turnInitialPosition = new Vector2(1000f, 310f);
+    private Vector2 turnFinalPosition;
+    private float turnTweenDuration = 1.6f;
+    private float turnFloatSpeed = .8f;
+    private float turnFloatAmplitude = 12f;
+    private bool turnHasEntered;
+
+    // ------------------------------------------------------------------------------------------------------
+
+    [SerializeField] private RectTransform scorePopInText;
+    private Vector2 scorePopInInitialPosition = new Vector2(0f, 0f);    
+    private Vector2 scorePopInFinalPosition = new Vector2(0f, 80f);    
+    private float scorePopInTweenDuration = .5f;
+        
+    // ------------------------------------------------------------------------------------------------------
+    
+
+    public void OnScorePopInEnter()
+    {
+        float rotationValue = Random.Range(-5f, 5f);
+        TextMeshProUGUI textComponent = scorePopInText.GetComponent<TextMeshProUGUI>();
+        // reset
+        scorePopInText.DOAnchorPosY(scorePopInInitialPosition.y, 0);
+        textComponent.DOColor(new Color(textComponent.color.r, textComponent.color.g, textComponent.color.b, 1), 0);
+
+        // animate
+        scorePopInText.localRotation = Quaternion.Euler(0, 0, rotationValue);
+        scorePopInText.DOAnchorPosY(scorePopInFinalPosition.y, scorePopInTweenDuration);
+        textComponent.DOColor(new Color(textComponent.color.r, textComponent.color.g, textComponent.color.b, 0), fadeOutDuration);
+
+    }
+
+    public void OnTicTacToeBoardEnter()
+    {
+        ticTacToeBoard.DOAnchorPosY(ticTacToeBoardFinalPositionY, ticTacToeBoardTweenDuration);
+    }
+
+
+    private void OnExitButtonEnter()
+    {
+        exitButton.DOAnchorPosX(exitButtonFinalPosition.x, buttonTweenDuration).OnComplete(() => exitButtonHasEntered = true);
+    }
+
+    private void ExitButtonFloat()
+    {
+        if (!exitButtonHasEntered) return;
+        float newY = exitButtonFinalPosition.y + Mathf.Sin(Time.time * exitButtonFloatSpeed) * exitButtonFloatAmplitude;
+        exitButton.anchoredPosition = new Vector2(exitButtonFinalPosition.x, newY);
+    }
+    
+
+    private void OnTurnEnter()
+    {
+        turn.DOAnchorPosX(turnFinalPosition.x, turnTweenDuration).OnComplete(() => turnHasEntered = true);
+    }
+
+    private void TurnFloat()
+    {
+        if (!turnHasEntered) return;
+        float newY = turnFinalPosition.y + Mathf.Sin(Time.time * turnFloatSpeed) * turnFloatAmplitude;
+        turn.anchoredPosition = new Vector2(turnFinalPosition.x, newY);
+    }
+
+
+    private void OnScoreEnter()
+    {
+        score.DOAnchorPosX(scoreFinalPosition.x, scoreTweenDuration).OnComplete(() => scoreHasEntered = true);
+    }
+
+    private void ScoreFloat()
+    {
+        elapsedTime += Time.deltaTime;
+        if (!scoreHasEntered && elapsedTime < .5f) return;
+
+        float newY = scoreFinalPosition.y + Mathf.Sin(Time.time * scoreFloatSpeed) * scoreFloatAmplitude;
+        score.anchoredPosition = new Vector2(scoreFinalPosition.x, newY);
+
+        if (!isRotating)
+        {
+            float rotationValue = Random.Range(-5f, 5f);
+            isRotating = true;
+            score.DORotate(new Vector3(0, 0, rotationValue), 3f).OnComplete(() => isRotating = false);
+        }
+    }
+
+    void Start()
+    {
+        SetupInitialPosition();
+
+        OnTicTacToeBoardEnter();
+        OnExitButtonEnter();
+        OnScoreEnter();
+        OnTurnEnter();
+    }
+
+    void Update()
+    {
+        ExitButtonFloat();
+        ScoreFloat();
+        TurnFloat();
+    }
+
+    private void SetupInitialPosition()
+    {
+        Vector2 ticTacToepPosition = ticTacToeBoard.position;
+        ticTacToepPosition.y = ticTacToeBoardInitialPositionY;
+        ticTacToeBoard.position = ticTacToepPosition;
+
+        Vector2 scorePosition = score.position;
+        scorePosition.x = scoreInitialPosition.x;
+        score.position = scorePosition;
+
+        Vector2 turnPosition = turn.position;
+        turnPosition.x = turnInitialPosition.x;
+        turn.position = turnPosition;
+        
+        Vector2 exitPosition = exitButton.position;
+        exitPosition.x = exitButtonInitialPosition.x;
+        exitButton.position = exitPosition;
+
+        exitButton.anchoredPosition = new Vector2(exitButtonInitialPosition.x, exitButtonInitialPosition.y);
+    }
+
+}
---
+++ Scripts/Animation/AnimationManagerMainMenu.cs
@@ -0,0 +1,119 @@
+using UnityEngine;
+using TMPro;
+using DG.Tweening;
+
+public class AnimationManagerMainMenu : MonoBehaviour
+{
+    [SerializeField] private RectTransform startButton;
+    private float startButtonTweenDuration = 1.5f;
+    private float startButtonInitialPosition = -520f;
+    private float startButtonFinalPosition = 0f;
+    private bool startButtonHasEntered;
+    private float startButtonFloatSpeed = .5f;
+    private float startButtonFloatAmplitude = 15f;
+
+    [SerializeField] private RectTransform gameNamePanel;
+    private float gameNameTweenDuration = .7f;
+    private float gameNameInitialPosition = 1000f;
+    private float gameNameFinalPosition = 0f;
+
+    [SerializeField] private RectTransform difficultyPanel;
+    private float difficultyTweenDuration = .9f;
+    private float difficultyInitialPosition = -5020f;
+    private float difficultyFinalPosition = -100f;
+
+
+    [SerializeField] private TMP_Text floatingText; 
+    private float charFloatSpeed = 2f;             
+    private float charFloatAmplitude = 10f;       
+    private bool isAnimatingCharacters = true;    
+
+    private float elapsedTime = 0f;
+
+
+    private void Start()
+    {
+        SetupInitialPosition();
+        OnStartEnter();
+        OnGameNameEnter();
+    }
+
+    private void Update()
+    {
+        OnDifficultyEnter();
+        StartButtonFloat();
+
+        if (isAnimatingCharacters)
+        {
+            AnimateFloatingText();
+        }
+    }
+
+    private void SetupInitialPosition()
+    {
+        startButton.anchoredPosition = new Vector2(startButton.anchoredPosition.y, startButtonInitialPosition);
+        difficultyPanel.anchoredPosition = new Vector2(difficultyPanel.anchoredPosition.x, difficultyInitialPosition);
+
+        Vector2 gameNamePosition = gameNamePanel.anchoredPosition;
+        gameNamePosition.x = -gameNameInitialPosition * 2;
+        gameNamePanel.anchoredPosition = gameNamePosition;
+    }
+
+    public void OnStartEnter()
+    {
+        startButton.DOAnchorPosY(startButtonFinalPosition, startButtonTweenDuration).OnComplete(() => startButtonHasEntered = true);
+    }
+
+    private void StartButtonFloat()
+    {
+        if (!startButtonHasEntered) return;
+        float newY = startButtonFinalPosition + Mathf.Sin(Time.time * startButtonFloatSpeed) * startButtonFloatAmplitude;
+        startButton.anchoredPosition = new Vector2(startButton.anchoredPosition.x, newY);
+    }
+
+    public void OnGameNameEnter()
+    {
+            gameNamePanel.DOAnchorPosX(gameNameFinalPosition, gameNameTweenDuration);
+    }
+
+    private void AnimateFloatingText()
+    {
+        floatingText.ForceMeshUpdate();
+        TMP_TextInfo textInfo = floatingText.textInfo;
+
+        for (int i = 0; i < textInfo.characterCount; i++)
+        {
+            TMP_CharacterInfo charInfo = textInfo.characterInfo[i];
+
+            // Get the vertices of the character
+            int vertexIndex = charInfo.vertexIndex;
+            int materialIndex = charInfo.materialReferenceIndex;
+            Vector3[] vertices = textInfo.meshInfo[materialIndex].vertices;
+
+            // Calculate the floating effect
+            float offset = Mathf.Sin(Time.time * charFloatSpeed + i * 0.3f) * charFloatAmplitude;
+
+            // Apply the floating effect
+            vertices[vertexIndex + 0].y += offset;
+            vertices[vertexIndex + 1].y += offset;
+            vertices[vertexIndex + 2].y += offset;
+            vertices[vertexIndex + 3].y += offset;
+        }
+
+        for (int i = 0; i < textInfo.meshInfo.Length; i++)
+        {
+            TMP_MeshInfo meshInfo = textInfo.meshInfo[i];
+            meshInfo.mesh.vertices = meshInfo.vertices;
+            floatingText.UpdateGeometry(meshInfo.mesh, i);
+        }
+    }
+
+    private void OnDifficultyEnter()
+    {
+        elapsedTime += Time.deltaTime;
+        if (elapsedTime >= .8f)
+        {
+            difficultyPanel.DOAnchorPosY(difficultyFinalPosition, difficultyTweenDuration);
+        }
+    }
+}
